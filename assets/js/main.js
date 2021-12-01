8e8c3719794b9654c6883c549b23f38adaa83d9b
Schmelzofen <sertac.oezdemir@gmail.com>
2021-12-01 21:02:40 +0100

    counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,13 @@
 let range = document.getElementsByClassName("slider")
 let demo = document.getElementById("demo")
+let count = 0
 
 let changeBackground = (x) => {
     document.body.style.backgroundColor = x
+    demo.innerHTML = `Sie haben die Farbe bereits ${count+=1} mal geändert.`
 }
 
 changeHandle = () => {
-    console.log(range[0].value)
-    console.log(range[1].value)
-    console.log(range[2].value)
     document.body.style.backgroundColor = `rgb(${range[0].value},${range[1].value},${range[2].value})`
     demo.innerHTML = `rgb(${range[0].value},${range[1].value},${range[2].value})=0x${(1<<24|range[0].value<<16|range[1].value<<8|range[2].value).toString(16).slice(1)}`
 }
-
-console.log((1<<24|255<<16|200<<8|100).toString(16).slice(1))
